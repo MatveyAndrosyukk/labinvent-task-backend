c7f52745a19388e318adfaad02e474604ab946b7
Matvey <matveyhf@gmail.com>
2022-11-30 00:17:55 +0300

    imports

--- src/main/java/com/example/labinventtaskbackend/config/SecurityConfig.java
+++ src/main/java/com/example/labinventtaskbackend/config/SecurityConfig.java
@@ -1,39 +1,21 @@
 package com.example.labinventtaskbackend.config;
 
-import com.example.labinventtaskbackend.models.Role;
-import com.example.labinventtaskbackend.models.User;
 import com.example.labinventtaskbackend.security.jwt.JwtConfigurer;
 import com.example.labinventtaskbackend.security.jwt.JwtTokenProvider;
 import com.example.labinventtaskbackend.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Configuration
 public class SecurityConfig{
     private final JwtTokenProvider jwtTokenProvider;
 
-    private final UserService userService;
-
-    private final BCryptPasswordEncoder passwordEncoder;
-
-    public SecurityConfig(JwtTokenProvider jwtTokenProvider, UserService userService, BCryptPasswordEncoder passwordEncoder) {
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
-        this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Bean
